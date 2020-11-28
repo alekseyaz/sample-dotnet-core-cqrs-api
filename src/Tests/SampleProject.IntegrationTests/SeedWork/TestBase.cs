3e8502a2e465ce716bb1ec374ab8ed5c5d319098
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2020-11-28 10:16:47 +0300

    добавил механизм установки переменных окружения из launchSettings.json для app tests

--- src/Tests/SampleProject.IntegrationTests/SeedWork/TestBase.cs
+++ src/Tests/SampleProject.IntegrationTests/SeedWork/TestBase.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NSubstitute;
 using NUnit.Framework;
 using SampleProject.Application.Configuration.Emails;
@@ -27,6 +31,27 @@ namespace SampleProject.IntegrationTests.SeedWork
         {
             const string connectionStringEnvironmentVariable =
                 "ASPNETCORE_SampleProject_IntegrationTests_ConnectionString";
+
+            using (var file = File.OpenText("Properties\\launchSettings.json"))
+            {
+                var reader = new JsonTextReader(file);
+                var jObject = JObject.Load(reader);
+
+                var variables = jObject
+                    .GetValue("profiles")
+                    //select a proper profile here
+                    .SelectMany(profiles => profiles.Children())
+                    .SelectMany(profile => profile.Children<JProperty>())
+                    .Where(prop => prop.Name == "environmentVariables")
+                    .SelectMany(prop => prop.Value.Children<JProperty>())
+                    .ToList();
+
+                foreach (var variable in variables)
+                {
+                    Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
+                }
+            }
+
             ConnectionString = Environment.GetEnvironmentVariable(connectionStringEnvironmentVariable);
             if (ConnectionString == null)
             {
